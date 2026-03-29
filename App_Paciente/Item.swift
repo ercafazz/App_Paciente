757eccb9ad3c98d8887cfa37be11d218cd9be4d7
ercafazz <ecarmona@correo.unimet.edu.ve>
2026-03-28 20:37:41 -0400

    Initial Commit

---
+++ App_Paciente/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  App_Paciente
+//
+//  Created by Ernesto Carmona Fazzolari on 3/28/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
